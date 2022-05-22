7fe819f94682f1e30f0ed9c8a2271732146b547e
Johnny <Johnny.gu.tcg@gmail.com>
2022-05-22 16:00:07 +0800

    HashTable Practice

---
+++ ConsoleApp1/HashTablePractice/FirstRecurringNumber.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1.HashTablePractice
+{
+    class FirstRecurringNumber
+    {
+        //static void Main(string[] args)
+        //{
+        //    var aaa = new int[] { 2, 1, 1, 2, 3, 5, 1, 2, 4 };
+        //    //var aaa = new int[] { 2, 3,4,5 };
+        //    Console.WriteLine(FirstNumber1(aaa));
+        //    Console.WriteLine(FirstNumber2(aaa));
+        //}
+
+        public static int? FirstNumber2(int[] array)
+        {
+            //var h = new Hashtable();
+            //foreach(var item in array)
+            //{
+
+            //    if(h.ContainsKey(item))
+            //    {
+            //        return (int)h[item];
+            //    }
+            //    else
+            //    {
+            //        h[item] = item;
+            //    }
+            //}
+
+
+            var dictionarys = new Dictionary<int,int>();
+            foreach (var item in array)
+            {
+
+                if (dictionarys.TryGetValue(item,out int value))
+                {
+                    return value;
+                }
+                else
+                {
+                    dictionarys.Add(item, item);
+                }
+            }
+            return null;
+        }
+
+        public static int? FirstNumber1(int[] array)
+        {
+            for(var i =0;i<array.Length;i++)
+            {
+                for(var j=i+1;j<array.Length;j++)
+                {
+                    if (array[i] == array[j]) return array[i];
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ ConsoleApp1/HashTablePractice/HashImplement.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1.HashTablePractice
+{
+    public class MyNode
+    {
+        public MyNode(string key, int value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; set; }
+        public int Value { get; set; }
+
+    }
+
+
+    class HashTableI
+    {
+        //private int size;
+        private class MyNodes : List<MyNode>{};
+        private readonly int length;
+        private readonly MyNodes[] data;
+        public HashTableI(int length)
+        {
+            this.length = length;
+            data = new MyNodes[length];
+        }
+
+        private int Hash(string key)
+        {
+            int hash = 0;
+            for(var i=0;i<key.Length;i++)
+            {
+                hash = (hash + key[i] * i) % length;
+            }
+            return hash;
+        }
+
+        public void Set(string key,int value)
+        {
+            var address = Hash(key);
+            if(data[address] == null)
+            {
+                data[address] = new MyNodes();
+            }
+            data[address].Add(new MyNode(key, value));
+        }
+
+        public int Get(string key)
+        {
+            var address = Hash(key);
+            var current = data[address];
+            if(current != null && current.Any())
+            {
+                //大部分情況下視為O(1) => 無碰撞的情形之下
+               foreach(var item in current)
+                {
+                    if(item.Key.Equals(key))
+                    {
+                        return item.Value;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public List<string> Keys()
+        {
+            var newList = new List<string>();
+            foreach(var item in data)
+            {
+                if(item != null)
+                {
+                    newList.AddRange(item.Select(x => x.Key));
+                }    
+            }
+            return newList;
+        }
+
+    }
+    class HashImplement
+    {
+        //static void Main(string[] args)
+        //{
+        //    HashTable h = new HashTable(200000);
+        //    h.Set("grapes", 10000);
+        //    h.Set("apple", 54);
+        //    h.Set("banana", 520);
+        //    var value = h.Get("banana");
+        //    Console.WriteLine(value);
+        //    var list = h.Keys();
+        //    Console.WriteLine(h.Keys());
+        //}
+    }
+}
