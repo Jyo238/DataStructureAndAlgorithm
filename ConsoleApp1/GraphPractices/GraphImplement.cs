6b67aea01d294f614f0624a6294a60f67797d600
Johnny <Johnny.gu.tcg@gmail.com>
2022-05-28 01:27:01 +0800

    recusive - factorial

--- ConsoleApp1/GraphPractices/GraphImplement.cs
+++ ConsoleApp1/GraphPractices/GraphImplement.cs
@@ -57,25 +57,25 @@ namespace ConsoleApp1.GraphPractices
 
     internal class GraphImplement
     {
-        static void Main()
-        {
-            var myGraph = new MyGraph();
-            myGraph.AddVertex(0);
-            myGraph.AddVertex(1);
-            myGraph.AddVertex(2);
-            myGraph.AddVertex(3);
-            myGraph.AddVertex(4);
-            myGraph.AddVertex(5);
-            myGraph.AddVertex(6);
-            myGraph.AddEdge(3, 1);
-            myGraph.AddEdge(3, 4);
-            myGraph.AddEdge(4, 2);
-            myGraph.AddEdge(4, 5);
-            myGraph.AddEdge(1, 2);
-            myGraph.AddEdge(1, 0);
-            myGraph.AddEdge(0, 2);
-            myGraph.AddEdge(6, 5);
-            myGraph.ShowConnections();
-        }
+        //static void Main()
+        //{
+        //    var myGraph = new MyGraph();
+        //    myGraph.AddVertex(0);
+        //    myGraph.AddVertex(1);
+        //    myGraph.AddVertex(2);
+        //    myGraph.AddVertex(3);
+        //    myGraph.AddVertex(4);
+        //    myGraph.AddVertex(5);
+        //    myGraph.AddVertex(6);
+        //    myGraph.AddEdge(3, 1);
+        //    myGraph.AddEdge(3, 4);
+        //    myGraph.AddEdge(4, 2);
+        //    myGraph.AddEdge(4, 5);
+        //    myGraph.AddEdge(1, 2);
+        //    myGraph.AddEdge(1, 0);
+        //    myGraph.AddEdge(0, 2);
+        //    myGraph.AddEdge(6, 5);
+        //    myGraph.ShowConnections();
+        //}
     }
 }
---
+++ ConsoleApp1/RecursivePractices/FindFactorial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.RecursivePractices
+{
+    class FindFactorial
+    {
+        public static long FindFactorialRecusive(int number)
+        {
+            if(number == 1)
+            {
+                return 1;
+            }
+            return number * FindFactorialRecusive(number-1);
+        }
+
+        public static int FindFactorialIterative(int number)
+        {
+            if (number <= 1) return number;
+            var target = 1;
+            for(var i=0;i<number;i++)
+            {
+                target *= i+1;
+            }
+            return target;
+        }
+
+        //static void Main()
+        //{
+        //   var r1 = FindFactorialIterative(5);
+        //    var r2 = FindFactorialRecusive(5);
+        //    Console.WriteLine("OK");
+        //}
+    }
+}
